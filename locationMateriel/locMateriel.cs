915daed6f49ef8f95bcafd1b8f29befdfc74d138
Paola Costa <paola.costa98@gmail.com>
2021-02-12 12:16:26 +0100

    base du projet C#

---
+++ locationMateriel/locMateriel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace locationMateriel
+{
+    public partial class locMateriel : Form
+    {
+        public locMateriel()
+        {
+            InitializeComponent();
+        }
+
+        private void locMateriel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
