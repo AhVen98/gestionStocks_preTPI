6b9ce099f91ba2b77622d3877a0cadd984fd6b5f
Paola Costa <paola.costa98@gmail.com>
2021-02-18 15:17:46 +0100

    avancée base du prog
    fin de la création des constructeurs de classe

---
+++ locationMateriel/Clients.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace locationMateriel
+{
+    class Clients:Persons
+    {
+        private string firstName;
+        private string lastName;
+        private DateTime birthDate;
+        private string emailAdress;
+
+        public Clients(string firstName, string lastName, DateTime birthDate, string emailAdress) : base(firstName, lastName, birthDate, emailAdress)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.emailAdress = emailAdress;
+        }
+    }
+}
