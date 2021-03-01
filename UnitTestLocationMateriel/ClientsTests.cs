ca20bf52aa0ba06996bafa9b3a015018bedd5931
COSTA Paola <Paola.COSTA@cpnv.ch>
2021-03-01 11:34:59 +0100

    ajout d'un projet de test dans la solution

---
+++ UnitTestLocationMateriel/ClientsTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLocationMateriel
+{
+    [TestClass]
+    public class ClientsTest
+    {
+        [TestInitialize]
+        public void init()
+        {
+        
+        
+        }
+      
+        [TestMethod]
+        public void client_constructeur_OK()
+        {
+            
+
+        }
+    }
+}
