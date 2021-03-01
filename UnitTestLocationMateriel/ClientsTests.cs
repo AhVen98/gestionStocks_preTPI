c00e46b70a820d9c36033e99565a3fe993d2ae1d
Paola Costa <paola.costa98@gmail.com>
2021-03-01 12:16:19 +0100

    recherche de solution pour les classes de test

--- UnitTestLocationMateriel/ClientsTests.cs
+++ UnitTestLocationMateriel/ClientsTests.cs
@@ -1,14 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using locationMateriel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace UnitTestLocationMateriel
+
+namespace locationMateriel.Tests
 {
-    [TestClass]
-    public class ClientsTest
+    [TestClass()]
+    public class PersonsTests
     {
-        [TestInitialize]
-        public void init()
-        {
         
+        
+        [TestInitialize]
+        public void Init()
+        {
+            Persons client;
         
         }
       
@@ -16,7 +25,6 @@ namespace UnitTestLocationMateriel
         public void client_constructeur_OK()
         {
             
-
         }
     }
 }
---
+++ locationMaterielTests/ObjectsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using locationMateriel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace locationMateriel.Tests
+{
+    [TestClass()]
+    public class ObjectsTests
+    {
+        Objects testObject;
+        [TestInitialize]
+        public void Init()
+        {
+            testObject = new Objects();
+        }
+
+        [TestMethod]
+        public void addObjects_allData_OK()
+        {
+            bool resExpected = true;
+
+            bool resCalculated = AddObject(testObject);
+
+            Assert.AreEqual(resExpected, resCalculated);
+        }
+    }
+}
