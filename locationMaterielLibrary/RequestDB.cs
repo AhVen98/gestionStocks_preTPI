d46b436b8755db48ff061595289372bd722e5328
Paola Costa <paola.costa98@gmail.com>
2021-03-05 09:30:18 +0100

    premières requêtes SQL rédigées

--- locationMaterielLibrary/RequestDB.cs
+++ locationMaterielLibrary/RequestDB.cs
@@ -7,6 +7,7 @@ namespace locationMaterielLibrary
 {
     public class RequestDB
     {
+        MySqlCommand cmd;
         /**
                 // Requête SQL
                 cmd.CommandText = "INSERT INTO objects (id, name, tel) VALUES (@id, @name, @tel)";
@@ -16,8 +17,27 @@ namespace locationMaterielLibrary
                 cmd.Parameters.AddWithValue("@name", newObject.Name);
                 cmd.Parameters.AddWithValue("@tel", newObject.Tel);
          */
+        public MySqlCommand ShowObject(int id, bool state)
+        {
+            // the object is available to rent (not rented)
+            if (state == true)
+            {
+                cmd.CommandText = "SELECT objects.name, objects.state, persons.firstname persons.lastname, locations.endDateLocation, types.name FROM types, objects, locations, persons WHERE objects.id =@id AND locations.locator_id = persons.id AND types.id = objects.type_id";
+            }
+            // the object is unavailable to rent (already rented)
+            else
+            {
+                cmd.CommandText = "SELECT objects.name, objects.state, types.name FROM types, objects, WHERE objects.id=@id AND types.id = objects.type_id";
+            }
+            cmd.Parameters.AddWithValue("@id", id);
+            return cmd;
+        }
 
-
-
+        public MySqlCommand GetStateFromObject(int id)
+        {
+            cmd.CommandText = "SELECT objects.state FROM objects WHERE objects.id =@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            return cmd;
+        }
     }
 }
