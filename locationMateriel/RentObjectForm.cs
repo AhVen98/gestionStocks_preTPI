4e52c51597f66bc6f003bbb22248905d41d04661
Paola Costa <paola.costa98@gmail.com>
2021-03-08 15:29:01 +0100

    création du formulaire pour la location d'un objet

---
+++ locationMateriel/RentObjectForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace locationMateriel
+{
+    public partial class frmRentObject : Form
+    {
+        public frmRentObject()
+        {
+            InitializeComponent();
+        }
+    }
+}
